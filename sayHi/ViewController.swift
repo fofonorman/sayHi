c49f8196fbd1dd48fef6471f3d6bd10fe2a6ec62
PanTing <fofonorman@gmail.com>
2024-05-01 10:02:10 +0800

    Initial Commit

---
+++ sayHi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sayHi
+//
+//  Created by ting pan on 2024/5/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
